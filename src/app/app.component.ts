54d98c4ca183016373b7d9438cb90c5d590fd96d
yasharsanayee <yasharsanayee@gmail.com>
2023-07-15 01:09:46 +0330

    moved files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { LayoutComponent } from './comps/layout/layout/layout.component';
+import { LayoutComponent } from './comps/layout/layout.component';
 
 @Component({
   selector: 'app-root',
