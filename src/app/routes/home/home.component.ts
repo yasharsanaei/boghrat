a522bc79a6086930c464e8088ffd99b9d9a4ebb1
yasharsanayee <yasharsanayee@gmail.com>
2023-07-15 02:00:53 +0330

    fixed ui

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatListModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
